5a6b35a338c56f54b66bc8dc972158a617d86ec4
Suyash Joshi <sjoshi@Suyashs-MacBook-Pro.local>
2024-07-23 17:00:33 +0100

    Created simple java app with InfluxDB Serverless integration

---
+++ src/main/java/com/influxdata/demo/HelloInfluxDB.java
@@ -0,0 +1,12 @@
+package com.influxdata.demo;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+  public static void main(String[] args) {
+
+    This information can be found in the InfluxDB UI
+
+    }
+  }
+}
